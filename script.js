0c1b878855e8eb33a6e65807702866d11bc79fdc
johanito <ufcmjohan@gmail.com>
2023-11-19 11:20:04 +0100

    responsive fix

--- script.js
+++ script.js
@@ -1,22 +1,19 @@
 document.addEventListener('DOMContentLoaded', function () {
-  // ... (votre code existant)
-
   var carousel = document.querySelector("#carousel");
   var slides = carousel.querySelectorAll(".slide");
-
   var currentSlide = 0;
   var previousSlide = slides.length - 1;
 
   function nextSlide() {
-    currentSlide++;
-    if (currentSlide >= slides.length) {
-      currentSlide = 0;
-    }
+      currentSlide++;
+      if (currentSlide >= slides.length) {
+          currentSlide = 0;
+      }
 
-    slides[currentSlide].style.display = "flex";
-    slides[previousSlide].style.display = "none";
+      slides[currentSlide].style.display = "flex";
+      slides[previousSlide].style.display = "none";
 
-    previousSlide = currentSlide;
+      previousSlide = currentSlide;
   }
 
   setInterval(nextSlide, 2710);
@@ -24,24 +21,12 @@ document.addEventListener('DOMContentLoaded', function () {
   slides[currentSlide].style.display = "flex";
   slides[previousSlide].style.display = "none";
 
-  document.getElementById("logo").addEventListener("click", function() {
-    window.location.href = "index.html";
+  document.getElementById("logo").addEventListener("click", function () {
+      window.location.href = "index.html";
   });
 
-  document.getElementById("logo").addEventListener("mouseover", function() {
+  document.getElementById("logo").addEventListener("mouseover", function () {
       document.getElementById("logo").classList.add("cursor-hand");
-  });
-
-  const links = document.querySelectorAll("nav li");
-
-  icons.addEventListener("click", () => {
-    nav.classList.toggle("active");
-  });
-
-  links.forEach((link) => {
-    link.addEventListener("click", () => {
-      nav.classList.remove("active");
-    });
   });
 
   const cardsContainer = document.querySelector('.cards-container');
@@ -49,30 +34,44 @@ document.addEventListener('DOMContentLoaded', function () {
   let currentIndex = 0;
 
   function showNextCard() {
-    currentIndex++;
-    if (currentIndex >= cardsContainer.children.length) {
-      currentIndex = 0;
-    }
-    updateSliderPosition();
+      currentIndex++;
+      if (currentIndex >= cardsContainer.children.length) {
+          currentIndex = 0;
+      }
+      updateSliderPosition();
   }
 
   function showPreviousCard() {
-    currentIndex--;
-    if (currentIndex < 0) {
-      currentIndex = cardsContainer.children.length - 1;
-    }
-    updateSliderPosition();
+      currentIndex--;
+      if (currentIndex < 0) {
+          currentIndex = cardsContainer.children.length - 1;
+      }
+      updateSliderPosition();
   }
 
   function updateSliderPosition() {
-    const newPosition = -currentIndex * cardWidth;
-    cardsContainer.style.transform = `translateX(${newPosition}px)`;
+      const newPosition = -currentIndex * cardWidth;
+      cardsContainer.style.transform = `translateX(${newPosition}px)`;
   }
 
-  // Utilisez les boutons "Next" et "Previous" pour déclencher le défilement
   const nextButton = document.getElementById('next');
   const prevButton = document.getElementById('prev');
 
   nextButton.addEventListener('click', showNextCard);
   prevButton.addEventListener('click', showPreviousCard);
+});
+
+const nav = document.getElementById('nav');
+const icons = document.getElementById('icons');
+
+icons.addEventListener("click", () => {
+  nav.classList.toggle("active");
+});
+
+const links = document.querySelectorAll("nav li");
+
+links.forEach((link) => {
+  link.addEventListener("click", () => {
+      nav.classList.remove("active");
+  });
 });
